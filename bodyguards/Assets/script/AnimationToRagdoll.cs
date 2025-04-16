05174e1498578c4eedde5b43b3a05374e1041994
Luan-da-Silva-Alves <89282026+Luan-da-Silva-Alves@users.noreply.github.com>
2025-04-16 20:35:57 -0300

    funcionalidade do projectille

---
+++ bodyguards/Assets/script/AnimationToRagdoll.cs
@@ -0,0 +1,71 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationToRagdoll : MonoBehaviour
+{
+    [SerializeField] Collider myColider;
+    [SerializeField] float respawnTime = 30f;
+    Rigidbody[] rigidbodies;
+    bool bIsRagdoll = false;
+
+    void Start()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+        ToggleRagdoll(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"colisao detectada com:" +
+            $"- tag: {collision.gameObject.tag} - bIsRagdoll: " +
+            $"{bIsRagdoll}");
+
+        if (collision.gameObject.CompareTag("Projectile")
+            && !bIsRagdoll)
+        {
+            Debug.Log("Colidiu com projetil");
+            ToggleRagdoll(false);
+        }
+    }
+    private void ToggleRagdoll(bool bisAnimating)
+    {
+        bIsRagdoll = !bisAnimating;
+        myColider.enabled = bisAnimating;
+
+        foreach (Rigidbody ragdollBone in rigidbodies)
+        {
+            Debug.Log($"{ragdollBone} isKinematic  =" +
+                $"{ragdollBone.isKinematic}");
+        }
+
+        foreach(Rigidbody ragdollBone in rigidbodies){
+            ragdollBone.isKinematic = bisAnimating; }
+
+        GetComponent<Animator>().enabled = bisAnimating;
+        if (bisAnimating) { RandomAnimation(); }
+    }
+
+    void RandomAnimation()
+    {
+        int randomNum = UnityEngine.Random.Range(0, 2);
+        Debug.Log(randomNum);
+        Animator animator = GetComponent<Animator>();
+
+        if (randomNum == 0)
+        {
+            animator.SetTrigger("Walk");
+        }
+        else
+        {
+            animator.SetTrigger("Idle");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
